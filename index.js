a1aaa3625a3c817afe587e8e9a5024789c5476bf
Anton Morev <shikto@mail.ru>
2021-08-12 20:16:24 +0300

    Env connected creds

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Bye Bye World!')
-})
+// RUN LIKE THIS
+// DB_USER=root DB_PASSWORD=123123 npm run dev
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log({dbUser, dbPassword})
+// Креды - Creds - Credentials - Данные для доступа куда либо - авторизационные данные
+//
+// app.get('/', (req, res) => {
+//   res.send('Bye Bye World!')
+// })
+//
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`)
+// })
