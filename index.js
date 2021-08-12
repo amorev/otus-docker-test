58b58b5e97476ceb27152356da5968552440d77b
Anton Morev <shikto@mail.ru>
2021-08-12 20:20:53 +0300

    Port to dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT
 
 // RUN LIKE THIS
 // DB_USER=root DB_PASSWORD=123123 npm run dev
