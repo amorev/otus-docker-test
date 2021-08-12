c5b96f235e9301cd8f8561fe5605443f01a4732f
Anton Morev <shikto@mail.ru>
2021-08-12 20:23:49 +0300

    Config file with dotenv

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "sdasd": process.env.ASDASD || '111',
+  "asdasdas": 123123,
+  "dasdsad": {
+    "asdsad": {
+      "asdfsdfgsdf": {
+        "sdfsdfasfda": '123123'
+      }
+    }
+  }
+}
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config()
+const config = require('./config')
+console.log(config)
 const express = require('express')
 const app = express()
 const port = process.env.PORT
