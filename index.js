56bb9a3e1cbb460cba6c8dae187c2ada3738abda
Anton Morev <shikto@mail.ru>
2021-10-20 21:16:29 +0300

    Docker-compose

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const fs = require('fs')
 
 app.get('/', (req, res) => {
+  fs.writeFileSync('data/info.txt', (new Date).toISOString())
   res.send('Bye Bye Bye World!')
 })
 
